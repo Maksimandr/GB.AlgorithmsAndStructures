d0d9a506a50b030121a9fcd85b70713c0bed6ea4
Maksimandr <maksimandr@mail.ru>
2021-08-13 20:01:51 +0500

    lesson 5 оптимизировал Exponentiation с использованием четности степени

--- src/main/java/lesson5/Exponentiation.java
+++ src/main/java/lesson5/Exponentiation.java
@@ -7,9 +7,15 @@ public class Exponentiation {
 
     public static int calculate(int base, int index) {
 
+        System.out.println("*");
         if (index == 1) {
             return base;
+        } else if (index % 2 == 1) {
+            return base * calculate(base, index - 1);
+        } else {
+            int x = calculate(base, index / 2);// если степень четная, то можно сократить глубину рекурсии
+            return x * x;
         }
-        return base * calculate(base, index - 1);
+
     }
 }
