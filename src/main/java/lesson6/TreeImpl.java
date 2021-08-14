44762bc9c3a3436e7bae7a22b7bbf974926763c8
Maksimandr <maksimandr@mail.ru>
2021-08-14 23:32:15 +0500

    lesson 6 выполнил задание

--- src/main/java/lesson6/TreeImpl.java
+++ src/main/java/lesson6/TreeImpl.java
@@ -264,4 +264,21 @@ public class TreeImpl<E extends Comparable<? super E>> implements Tree<E>{
         preOrder(current.getRightChild());
     }
 
+    /**
+     * Метод проверяет сбалансированность всего дерева
+     */
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    public static boolean isBalanced(Node node) {
+        return (node == null) ||
+                isBalanced(node.getLeftChild()) &&
+                        isBalanced(node.getRightChild()) &&
+                        Math.abs(height(node.getLeftChild()) - height(node.getRightChild())) <= 1;
+    }
+
+    private static int height(Node node) {
+        return node == null ? 0 : 1 + Math.max(height(node.getLeftChild()), height(node.getRightChild()));
+    }
 }
